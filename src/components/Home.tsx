a1b219d59a918aca504e557a994c7c6215ad157b
Hrikesh <hnadaraj@gmail.com>
2025-02-07 17:28:56 +0000

    Update Home.tsx
    change to image pointer

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -95,7 +95,7 @@ const Home = () => {
                   className="relative rounded-3xl overflow-hidden aspect-[4/5] bg-gradient-to-tr from-blue-500/10 to-purple-500/10 p-1"
                 >
                   <img
-                    src="/src/assets/images/profile2.JPG"
+                    src="https://github.com/N-Hrikeshraj/hrikesh/blob/main/src/assets/images/profile1.JPG"
                     alt="Hrikeshraj Nadaraj"
                     className="w-full h-full object-cover rounded-2xl"
                   />
